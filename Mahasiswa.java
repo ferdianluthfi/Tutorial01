66b0e6fcba66b705519625d87d957ad89427c823
Luthfi Ferdian <luthfi14ferdian@gmail.com>
2018-09-07 14:54:17 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+public class Mahasiswa{
+	public static void main(String[]args){
+		
+	}
+	
+	public void setNama(){
+		String nama;
+		this.nama = nama;
+	}
+	
+	public String getNama(){
+		System.out.println(this.nama);
+	}
+}
